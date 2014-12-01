ce68a7ba345385345c7e9b0c6227626208d7496a
karaAJC <KaraAJC@gmail.com>
2014-11-30 23:57:37 -0700

    updating cheatsheet project pages

--- stylesheets/assets/script.js
+++ stylesheets/assets/script.js
@@ -1,15 +1,42 @@
-// var main = function() {
-//     $('.ide').click(function() { 
-//     	console.log("you clicked div.ide");
+// /* /*var main = function () {
+//     $('.ide').click(function () {
+//         $('.description').toggle();
 //         $(this).next('.description').hide();
 //     });
-// }
+// };
 // $(document).ready(main);
 
 
-$(document).ready(function() {
-   $('.item').click(function() {
-   	$('.description').hide();
-    $(this).next('.description').show();
-   }); 
-});
+// $(document).ready(function () {
+//     $('.item').click(function () {
+//         $('.description').toggle();
+//         $(this).next('.description').show();
+//     });
+// });
+
+// var main = function () {
+//     /* Push the body and the nav over by 285px over */
+//     $('.item').click(function () {
+//         $('.description').animate({
+//             left: "0px"
+//         }, 200);
+
+//         $('body').animate({
+//             left: "285px"
+//         }, 200);
+//     });
+
+//     /* Then push them back */
+//     $('.item').click(function () {
+//         $('.description').animate({
+//             left: "-285px"
+//         }, 200);
+
+//         $('body').animate({
+//             left: "0px"
+//         }, 200);
+//     });
+// };
+
+
+// $(document).ready(main); */
